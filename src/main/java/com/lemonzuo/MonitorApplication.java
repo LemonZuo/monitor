6a0c39219c14cf2851f9536f403bea08455afbc6
LemonZuo <lemonzuo@qq.com>
2021-02-27 21:32:56 +0800

    monitor

---
+++ src/main/java/com/lemonzuo/MonitorApplication.java
@@ -0,0 +1,18 @@
+package com.lemonzuo;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 左铠琦
+ */
+@EnableAdminServer
+@SpringBootApplication
+public class MonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+    }
+
+}
